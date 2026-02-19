5fa25a255522c5e3a6cff9548b921614cbc6d61a
efahnjoe <efahnjoe@gmail.com>
2026-02-19 12:42:40 +0800

    refactor(og-image): disable dynamic OG image generation
    - Comment out import statements for APIRoute and CollectionEntry in index.png.ts
    - Remove generateOgImageForPost import and usage
    - Comment out the entire GET API route implementation
    - Comment out all functions in generateOgImages.ts including
      svgBufferToPngBuffer, generateOgImageForPost and generateOgImageForSite
    - Comment out satori imports and implementations in both post.js and site.js
      OG templates
    - This effectively disables the dynamic OG image generation feature

--- src/pages/posts/[...slug]/index.png.ts
+++ src/pages/posts/[...slug]/index.png.ts
@@ -1,7 +1,8 @@
-import type { APIRoute } from "astro";
-import { getCollection, type CollectionEntry } from "astro:content";
+// import type { APIRoute } from "astro";
+// import type { CollectionEntry } from "astro:content";
+import { getCollection } from "astro:content";
 import { getPath } from "@/utils/getPath";
-import { generateOgImageForPost } from "@/utils/generateOgImages";
+// import { generateOgImageForPost } from "@/utils/generateOgImages";
 import { SITE } from "@/config";
 
 export async function getStaticPaths() {
@@ -19,16 +20,16 @@ export async function getStaticPaths() {
   }));
 }
 
-export const GET: APIRoute = async ({ props }) => {
-  if (!SITE.dynamicOgImage) {
-    return new Response(null, {
-      status: 404,
-      statusText: "Not found",
-    });
-  }
+// export const GET: APIRoute = async ({ props }) => {
+//   if (!SITE.dynamicOgImage) {
+//     return new Response(null, {
+//       status: 404,
+//       statusText: "Not found",
+//     });
+//   }
 
-  const buffer = await generateOgImageForPost(props as CollectionEntry<"blog">);
-  return new Response(new Uint8Array(buffer), {
-    headers: { "Content-Type": "image/png" },
-  });
-};
+//   const buffer = await generateOgImageForPost(props as CollectionEntry<"blog">);
+//   return new Response(new Uint8Array(buffer), {
+//     headers: { "Content-Type": "image/png" },
+//   });
+// };
